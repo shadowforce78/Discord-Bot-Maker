28d8ee10be8eb443662dd3911c03766034e4854d
shadowforce78 <planque.adam@gmail.com>
2023-04-19 00:15:32 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace Discord_Bot_Maker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.formSkin1 = new FlatUI.FormSkin();
+            this.flatTabControl1 = new FlatUI.FlatTabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.flatButton2 = new FlatUI.FlatButton();
+            this.flatLabel2 = new FlatUI.FlatLabel();
+            this.flatLabel1 = new FlatUI.FlatLabel();
+            this.flatButton1 = new FlatUI.FlatButton();
+            this.flatTextBox3 = new FlatUI.FlatTextBox();
+            this.flatTextBox2 = new FlatUI.FlatTextBox();
+            this.flatTextBox1 = new FlatUI.FlatTextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.flatClose1 = new FlatUI.FlatClose();
+            this.flatMini1 = new FlatUI.FlatMini();
+            this.formSkin1.SuspendLayout();
+            this.flatTabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // formSkin1
+            // 
+            this.formSkin1.BackColor = System.Drawing.Color.White;
+            this.formSkin1.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(70)))), ((int)(((byte)(73)))));
+            this.formSkin1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(58)))), ((int)(((byte)(60)))));
+            this.formSkin1.Controls.Add(this.flatTabControl1);
+            this.formSkin1.Controls.Add(this.flatClose1);
+            this.formSkin1.Controls.Add(this.flatMini1);
+            this.formSkin1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.formSkin1.FlatColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(168)))), ((int)(((byte)(109)))));
+            this.formSkin1.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.formSkin1.HeaderColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(47)))), ((int)(((byte)(49)))));
+            this.formSkin1.HeaderMaximize = false;
+            this.formSkin1.Location = new System.Drawing.Point(0, 0);
+            this.formSkin1.Name = "formSkin1";
+            this.formSkin1.Size = new System.Drawing.Size(800, 450);
+            this.formSkin1.TabIndex = 0;
+            this.formSkin1.Text = "Discord Bot Maker";
+            // 
+            // flatTabControl1
+            // 
+            this.flatTabControl1.ActiveColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(168)))), ((int)(((byte)(109)))));
+            this.flatTabControl1.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(47)))), ((int)(((byte)(49)))));
+            this.flatTabControl1.Controls.Add(this.tabPage1);
+            this.flatTabControl1.Controls.Add(this.tabPage2);
+            this.flatTabControl1.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.flatTabControl1.ItemSize = new System.Drawing.Size(120, 40);
+            this.flatTabControl1.Location = new System.Drawing.Point(0, 72);
+            this.flatTabControl1.Name = "flatTabControl1";
+            this.flatTabControl1.SelectedIndex = 0;
+            this.flatTabControl1.Size = new System.Drawing.Size(797, 378);
+            this.flatTabControl1.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.flatTabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(70)))), ((int)(((byte)(73)))));
+            this.tabPage1.Controls.Add(this.flatButton2);
+            this.tabPage1.Controls.Add(this.flatLabel2);
+            this.tabPage1.Controls.Add(this.flatLabel1);
+            this.tabPage1.Controls.Add(this.flatButton1);
+            this.tabPage1.Controls.Add(this.flatTextBox3);
+            this.tabPage1.Controls.Add(this.flatTextBox2);
+            this.tabPage1.Controls.Add(this.flatTextBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 44);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(789, 330);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Maker";
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // flatButton2
+            // 
+            this.flatButton2.BackColor = System.Drawing.Color.Transparent;
+            this.flatButton2.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(168)))), ((int)(((byte)(109)))));
+            this.flatButton2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.flatButton2.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.flatButton2.Location = new System.Drawing.Point(654, 6);
+            this.flatButton2.Name = "flatButton2";
+            this.flatButton2.Rounded = false;
+            this.flatButton2.Size = new System.Drawing.Size(129, 40);
+            this.flatButton2.TabIndex = 6;
+            this.flatButton2.Text = "Open Bot Folder";
+            this.flatButton2.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(243)))));
+            this.flatButton2.UseCustomColor = false;
+            this.flatButton2.Click += new System.EventHandler(this.flatButton2_Click);
+            // 
+            // flatLabel2
+            // 
+            this.flatLabel2.AutoSize = true;
+            this.flatLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.flatLabel2.Font = new System.Drawing.Font("Segoe UI", 8F);
+            this.flatLabel2.ForeColor = System.Drawing.Color.White;
+            this.flatLabel2.Location = new System.Drawing.Point(88, 20);
+            this.flatLabel2.Name = "flatLabel2";
+            this.flatLabel2.Size = new System.Drawing.Size(38, 13);
+            this.flatLabel2.TabIndex = 5;
+            this.flatLabel2.Text = "NONE";
+            this.flatLabel2.Click += new System.EventHandler(this.flatLabel2_Click);
+            // 
+            // flatLabel1
+            // 
+            this.flatLabel1.AutoSize = true;
+            this.flatLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.flatLabel1.Font = new System.Drawing.Font("Segoe UI", 8F);
+            this.flatLabel1.ForeColor = System.Drawing.Color.White;
+            this.flatLabel1.Location = new System.Drawing.Point(22, 20);
+            this.flatLabel1.Name = "flatLabel1";
+            this.flatLabel1.Size = new System.Drawing.Size(66, 13);
+            this.flatLabel1.TabIndex = 4;
+            this.flatLabel1.Text = "Bot Folder :";
+            // 
+            // flatButton1
+            // 
+            this.flatButton1.BackColor = System.Drawing.Color.Transparent;
+            this.flatButton1.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(168)))), ((int)(((byte)(109)))));
+            this.flatButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.flatButton1.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.flatButton1.Location = new System.Drawing.Point(655, 284);
+            this.flatButton1.Name = "flatButton1";
+            this.flatButton1.Rounded = false;
+            this.flatButton1.Size = new System.Drawing.Size(129, 40);
+            this.flatButton1.TabIndex = 3;
+            this.flatButton1.Text = "Create !";
+            this.flatButton1.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(243)))));
+            this.flatButton1.UseCustomColor = false;
+            this.flatButton1.Click += new System.EventHandler(this.flatButton1_Click);
+            // 
+            // flatTextBox3
+            // 
+            this.flatTextBox3.BackColor = System.Drawing.Color.Transparent;
+            this.flatTextBox3.FocusOnHover = false;
+            this.flatTextBox3.Hint = "";
+            this.flatTextBox3.Location = new System.Drawing.Point(525, 212);
+            this.flatTextBox3.MaxLength = 32767;
+            this.flatTextBox3.Multiline = false;
+            this.flatTextBox3.Name = "flatTextBox3";
+            this.flatTextBox3.ReadOnly = false;
+            this.flatTextBox3.Size = new System.Drawing.Size(244, 29);
+            this.flatTextBox3.TabIndex = 2;
+            this.flatTextBox3.Text = "Dev ID";
+            this.flatTextBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.flatTextBox3.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.flatTextBox3.UseSystemPasswordChar = false;
+            this.flatTextBox3.TextChanged += new System.EventHandler(this.flatTextBox3_TextChanged);
+            // 
+            // flatTextBox2
+            // 
+            this.flatTextBox2.BackColor = System.Drawing.Color.Transparent;
+            this.flatTextBox2.FocusOnHover = false;
+            this.flatTextBox2.Hint = "";
+            this.flatTextBox2.Location = new System.Drawing.Point(275, 212);
+            this.flatTextBox2.MaxLength = 32767;
+            this.flatTextBox2.Multiline = false;
+            this.flatTextBox2.Name = "flatTextBox2";
+            this.flatTextBox2.ReadOnly = false;
+            this.flatTextBox2.Size = new System.Drawing.Size(244, 29);
+            this.flatTextBox2.TabIndex = 1;
+            this.flatTextBox2.Text = "Bot ID";
+            this.flatTextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.flatTextBox2.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.flatTextBox2.UseSystemPasswordChar = false;
+            this.flatTextBox2.TextChanged += new System.EventHandler(this.flatTextBox2_TextChanged);
+            // 
+            // flatTextBox1
+            // 
+            this.flatTextBox1.BackColor = System.Drawing.Color.Transparent;
+            this.flatTextBox1.FocusOnHover = false;
+            this.flatTextBox1.Hint = "";
+            this.flatTextBox1.Location = new System.Drawing.Point(25, 212);
+            this.flatTextBox1.MaxLength = 32767;
+            this.flatTextBox1.Multiline = false;
+            this.flatTextBox1.Name = "flatTextBox1";
+            this.flatTextBox1.ReadOnly = false;
+            this.flatTextBox1.Size = new System.Drawing.Size(244, 29);
+            this.flatTextBox1.TabIndex = 0;
+            this.flatTextBox1.Text = "Bot Token";
+            this.flatTextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.flatTextBox1.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.flatTextBox1.UseSystemPasswordChar = false;
+            this.flatTextBox1.TextChanged += new System.EventHandler(this.flatTextBox1_TextChanged);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(70)))), ((int)(((byte)(73)))));
+            this.tabPage2.Location = new System.Drawing.Point(4, 44);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(789, 330);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Plugins";
+            // 
+            // flatClose1
+            // 
+            this.flatClose1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.flatClose1.BackColor = System.Drawing.Color.White;
+            this.flatClose1.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.flatClose1.Font = new System.Drawing.Font("Marlett", 10F);
+            this.flatClose1.Location = new System.Drawing.Point(779, 12);
+            this.flatClose1.Name = "flatClose1";
+            this.flatClose1.Size = new System.Drawing.Size(18, 18);
+            this.flatClose1.TabIndex = 1;
+            this.flatClose1.Text = "flatClose1";
+            this.flatClose1.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(243)))));
+            // 
+            // flatMini1
+            // 
+            this.flatMini1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.flatMini1.BackColor = System.Drawing.Color.White;
+            this.flatMini1.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(47)))), ((int)(((byte)(49)))));
+            this.flatMini1.Font = new System.Drawing.Font("Marlett", 12F);
+            this.flatMini1.Location = new System.Drawing.Point(755, 12);
+            this.flatMini1.Name = "flatMini1";
+            this.flatMini1.Size = new System.Drawing.Size(18, 18);
+            this.flatMini1.TabIndex = 0;
+            this.flatMini1.Text = "flatMini1";
+            this.flatMini1.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(243)))));
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.formSkin1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bot Maker";
+            this.TransparencyKey = System.Drawing.Color.Fuchsia;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.formSkin1.ResumeLayout(false);
+            this.flatTabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private FlatUI.FormSkin formSkin1;
+        private FlatUI.FlatClose flatClose1;
+        private FlatUI.FlatMini flatMini1;
+        private FlatUI.FlatTabControl flatTabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private FlatUI.FlatTextBox flatTextBox3;
+        private FlatUI.FlatTextBox flatTextBox2;
+        private FlatUI.FlatTextBox flatTextBox1;
+        private FlatUI.FlatButton flatButton2;
+        private FlatUI.FlatLabel flatLabel2;
+        private FlatUI.FlatLabel flatLabel1;
+        private FlatUI.FlatButton flatButton1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Discord_Bot_Maker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        { }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void flatTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void flatTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void flatTextBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void flatButton2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog FBD = new FolderBrowserDialog();
+            if (FBD.ShowDialog() == DialogResult.OK)
+            {
+                flatLabel2.Text = FBD.SelectedPath;
+            }
+        }
+
+        private void flatLabel2_Click(object sender, EventArgs e)
+        {
+            // if text is not equal to NONE, then open the folder
+            if (flatLabel2.Text != "NONE")
+            {
+                System.Diagnostics.Process.Start(flatLabel2.Text);
+            }
+            // if mouse is over the label, then show the path
+            else if (flatLabel2.Text == "NONE")
+            {
+                MessageBox.Show("You have not selected a folder yet!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void flatButton1_Click(object sender, EventArgs e)
+        {
+            // if label is not equal to NONE, then create the bot
+            if (flatLabel2.Text == "NONE")
+            { 
+                MessageBox.Show("You have not selected a folder yet!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (flatLabel2.Text != "NONE")
+            {
+                // if textboxes are not empty, then create the bot
+                if (flatTextBox1.Text == "" || flatTextBox2.Text == "" || flatTextBox3.Text == "" || flatTextBox1.Text == "Bot Token" || flatTextBox2.Text == "Bot ID" || flatTextBox3.Text == "Dev ID")
+                {
+                    MessageBox.Show("You have not filled in all the textboxes!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else if (flatTextBox1.Text != "" || flatTextBox2.Text != "" || flatTextBox3.Text != "")
+                {
+                    // create the bot
+                    MessageBox.Show("Bot created!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    // open the folder
+                    System.Diagnostics.Process.Start(flatLabel2.Text);
+                }
+            
+            }
+        }
+    }
+}
